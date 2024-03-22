3e707510d93617142a3e230d36c2834be5a1b56b
fersho101 <corral.fdo@gmail>
2024-03-21 19:51:47 -0700

    Se mueve fortune cookie a modulo

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,9 @@
 const express = require("express");
 // const expressHandlebars = require('express-handlebars')
 const {engine: expressHandlebars} = require('express-handlebars')
+
+const fortune = require('./lib/fortune.js');
+
 const app = express();
 
 app.engine('handlebars', expressHandlebars({
@@ -26,18 +29,12 @@ app.get('/about', (req, res) => {
 
 app.get('/', (req, res) => res.render('home'))
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-  ]
+
   
 
 app.get('/about', (req, res) => {
   const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-  res.render('about', {fortune: randomFortune})
+  res.render('about', {fortune: fortune.getFortune()})
 })
 
 
